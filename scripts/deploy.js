3feb45816679c2b43b23626b8e9083779d08fa6e
coder <codenow8@gmail.com>
2023-07-13 19:30:52 +0545

    deploy script ethers.js fixed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,21 @@
 const hre = require("hardhat");
 
 // console.log(hre);
+
+async function main() {
+  const currentTimeStampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEARS_IN_SECONDS = 365 * 24 * 60 * 60;
+  const unlockedTime = currentTimeStampInSeconds + ONE_YEARS_IN_SECONDS;
+
+  const lockedAmount = hre.ethers.parseEther("1");
+
+  console.log(currentTimeStampInSeconds);
+  console.log(ONE_YEARS_IN_SECONDS);
+  console.log(unlockedTime);
+  console.log(lockedAmount);
+}
+
+main().catch((error) => {
+  console.log(error);
+  process.exitCode = 1;
+});
